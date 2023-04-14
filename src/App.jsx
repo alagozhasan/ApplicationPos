36abeca6cb9f340265f89b84db1141e7db334e49
Hasan ALAGOZ <123511537+alagozhasan@users.noreply.github.com>
2023-04-15 00:36:54 +0300

    last

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import HomePage from "../../../ApplicationPos/client/src/pages/HomePage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import CartPage from "../../../ApplicationPos/client/src/pages/CartPage";
+import NotFound from "../../../ApplicationPos/client/src/pages/NotFound";
+import BillPage from "../../../ApplicationPos/client/src/pages/BillPage";
+import CustomerPage from "../../../ApplicationPos/client/src/pages/CustomerPage";
+import StatisticsPage from "../../../ApplicationPos/client/src/pages/StatisticsPage";
+import Register from "../../../ApplicationPos/client/src/pages/auth/Register";
+import Login from "../../../ApplicationPos/client/src/pages/auth/Login";
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage/>} />
+                <Route path="/sepet" element={<CartPage/>} />
+                <Route path="/bills" element={<BillPage/>} />
+                <Route path="/*" element={<NotFound/>} />
+                <Route path="/musteriler" element={<CustomerPage/>} />
+                <Route path="/kayit" element={<Register/>} />
+                <Route path="/giris" element={<Login/>} />
+                <Route path="/istatistik" element={<StatisticsPage/>} />
+            </Routes>
+
+        </BrowserRouter>
+    );
+}
+
+export default App;
